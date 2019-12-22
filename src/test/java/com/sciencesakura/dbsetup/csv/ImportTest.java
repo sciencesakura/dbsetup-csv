c20466bd3842e1654222ba286b39979d5e86400d
sciencesakura <sakurasakurascience@gmail.com>
2019-12-22 17:23:07 +0900

    #3 Migrate to JUnit 5

--- src/test/java/com/sciencesakura/dbsetup/csv/ImportTest.java
+++ src/test/java/com/sciencesakura/dbsetup/csv/ImportTest.java
@@ -23,82 +23,109 @@
  */
 package com.sciencesakura.dbsetup.csv;
 
-import com.ninja_squad.dbsetup.Operations;
-import com.ninja_squad.dbsetup.bind.BinderConfiguration;
-import com.ninja_squad.dbsetup.operation.Insert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import com.ninja_squad.dbsetup.DbSetup;
+import com.ninja_squad.dbsetup.destination.Destination;
+import com.ninja_squad.dbsetup.destination.DriverManagerDestination;
+import com.ninja_squad.dbsetup.operation.Operation;
+import org.assertj.db.type.DateTimeValue;
+import org.assertj.db.type.DateValue;
+import org.assertj.db.type.Source;
+import org.assertj.db.type.Table;
+import org.assertj.db.type.TimeValue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+import static com.sciencesakura.dbsetup.csv.Import.csv;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.assertj.db.type.Table.Order.asc;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({Insert.class, Insert.Builder.class, Operations.class})
-public class ImportTest {
+class ImportTest {
 
-    @Test
-    public void import_csv_default_style() throws SQLException {
-        Connection connection = mock(Connection.class);
-        BinderConfiguration configuration = mock(BinderConfiguration.class);
-        Insert insert = mock(Insert.class);
-        doNothing().when(insert).execute(any(), any());
-        Insert.Builder ib = mock(Insert.Builder.class);
-        when(ib.build()).thenReturn(insert);
-        mockStatic(Operations.class);
-        when(Operations.insertInto(anyString())).thenReturn(ib);
+    private static final Table.Order[] ORDER_BY_A = new Table.Order[] { asc("a") };
 
-        Import importCsv = Import.csv("data/default.csv")
-                .into("table_name")
-                .build();
-        importCsv.execute(connection, configuration);
+    private static Destination destination;
 
-        verifyStatic(Operations.class);
-        Operations.insertInto("table_name");
-        verify(ib).columns("a", "b", "c");
-        verify(ib).values("a1", "b1", "c1");
-        verify(ib).values("a2", "b2", "c2");
-        verify(ib).values("a3", null, null);
-        verify(ib).build();
-        verify(insert).execute(connection, configuration);
+    private static Source source;
+
+    @BeforeAll
+    static void setUpClass() throws SQLException {
+        String url = "jdbc:h2:mem:ImportTest;DB_CLOSE_DELAY=-1";
+        destination = DriverManagerDestination.with(url, "sa", "");
+        source = new Source(url, "sa", "");
+        try (Connection conn = destination.getConnection(); Statement stmt = conn.createStatement()) {
+            stmt.addBatch("create table default_csv (a int, b decimal(7, 3), c date, d timestamp, e varchar(6), f boolean)");
+            stmt.addBatch("create table pgsql_tsv (a int, b decimal(7, 3), c date, d timestamp, e varchar(6), f boolean)");
+            stmt.executeBatch();
+        }
     }
 
     @Test
-    public void import_tsv_pgsql_style() throws SQLException {
-        Connection connection = mock(Connection.class);
-        BinderConfiguration configuration = mock(BinderConfiguration.class);
-        Insert insert = mock(Insert.class);
-        doNothing().when(insert).execute(any(), any());
-        Insert.Builder ib = mock(Insert.Builder.class);
-        when(ib.build()).thenReturn(insert);
-        mockStatic(Operations.class);
-        when(Operations.insertInto(anyString())).thenReturn(ib);
+    void default_csv() {
+        Operation operation = csv("data/default.csv").into("default_csv").build();
+        DbSetup dbSetup = new DbSetup(destination, operation);
+        dbSetup.launch();
+        assertThat(new Table(source, "default_csv", ORDER_BY_A))
+                // 1
+                .row()
+                .column("a").value().isEqualTo(100)
+                .column("b").value().isEqualTo(0.5)
+                .column("c").value().isEqualTo(DateValue.of(2019, 12, 1))
+                .column("d").value().isEqualTo(datetime(2019, 12, 1, 9, 30, 1))
+                .column("e").value().isEqualTo("hoge")
+                .column("f").value().isTrue()
+                // 2
+                .row()
+                .column("a").value().isEqualTo(200)
+                .column("b").value().isEqualTo(0.25)
+                .column("c").value().isEqualTo(DateValue.of(2019, 12, 2))
+                .column("d").value().isEqualTo(datetime(2019, 12, 2, 9, 30, 2))
+                .column("e").value().isNull()
+                .column("f").value().isFalse();
+    }
 
-        Import importCsv = Import.csv("data/pgsql.tsv")
-                .into("table_name")
-                .withHeader("a", "b", "c")
+    @Test
+    void pgsql_tsv() {
+        Operation operation = csv("data/pgsql.tsv")
+                .into("pgsql_tsv")
                 .withDelimiter('\t')
+                .withHeader("a", "b", "c", "d", "e", "f")
                 .withNullAs("\\N")
                 .build();
-        importCsv.execute(connection, configuration);
+        DbSetup dbSetup = new DbSetup(destination, operation);
+        dbSetup.launch();
+        assertThat(new Table(source, "pgsql_tsv", ORDER_BY_A))
+                // 1
+                .row()
+                .column("a").value().isEqualTo(100)
+                .column("b").value().isEqualTo(0.5)
+                .column("c").value().isEqualTo(DateValue.of(2019, 12, 1))
+                .column("d").value().isEqualTo(datetime(2019, 12, 1, 9, 30, 1))
+                .column("e").value().isEqualTo("hoge")
+                .column("f").value().isTrue()
+                // 2
+                .row()
+                .column("a").value().isEqualTo(200)
+                .column("b").value().isEqualTo(0.25)
+                .column("c").value().isEqualTo(DateValue.of(2019, 12, 2))
+                .column("d").value().isEqualTo(datetime(2019, 12, 2, 9, 30, 2))
+                .column("e").value().isNull()
+                .column("f").value().isFalse();
+    }
 
-        verifyStatic(Operations.class);
-        Operations.insertInto("table_name");
-        verify(ib).columns("a", "b", "c");
-        verify(ib).values("a1", "b1", "c1");
-        verify(ib).values("a2", "b2", "c2");
-        verify(ib).values("a3", null, null);
-        verify(ib).build();
-        verify(insert).execute(connection, configuration);
+    @Test
+    void file_not_found() {
+        assertThatThrownBy(() -> csv("data/file_not_found"))
+                .hasMessage("specified resource was not found: data/file_not_found");
+    }
+
+    private static DateTimeValue datetime(int year, int month, int dayOfMonth, int hours, int minutes, int seconds) {
+        DateValue d = DateValue.of(year, month, dayOfMonth);
+        TimeValue t = TimeValue.of(hours, minutes, seconds);
+        return DateTimeValue.of(d, t);
     }
 }
