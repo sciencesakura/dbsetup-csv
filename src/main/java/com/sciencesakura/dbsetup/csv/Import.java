fe1aee23602ba4492fa1bb9d547d8a2c27151605
sciencesakura <sakurasakurascience@gmail.com>
2019-12-24 00:21:49 +0900

    Add assertions

--- src/main/java/com/sciencesakura/dbsetup/csv/Import.java
+++ src/main/java/com/sciencesakura/dbsetup/csv/Import.java
@@ -24,7 +24,6 @@
 package com.sciencesakura.dbsetup.csv;
 
 import com.ninja_squad.dbsetup.DbSetupRuntimeException;
-import com.ninja_squad.dbsetup.Operations;
 import com.ninja_squad.dbsetup.bind.BinderConfiguration;
 import com.ninja_squad.dbsetup.operation.Insert;
 import com.ninja_squad.dbsetup.operation.Operation;
@@ -40,6 +39,9 @@ import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Collection;
+
+import static com.ninja_squad.dbsetup.Operations.insertInto;
+import static java.util.Objects.requireNonNull;
 
 /**
  * An operation which imports a CSV file into a table.
@@ -68,6 +70,7 @@ public class Import implements Operation {
      *
      * @param location a location of source file that is the relative path from classpath root
      * @return a new builder
+     * @throws IllegalArgumentException if the source file was not found
      */
     @NotNull
     public static Builder csv(@NotNull String location) {
@@ -91,7 +94,7 @@ public class Import implements Operation {
 
     @Override
     public void execute(Connection connection, BinderConfiguration configuration) throws SQLException {
-        Insert.Builder ib = Operations.insertInto(table);
+        Insert.Builder ib = insertInto(table);
         try (CSVParser csv = CSVParser.parse(location.openStream(), charset, format)) {
             ib.columns(csv.getHeaderNames().toArray(EMPTY_ARRAY));
             csv.forEach(row -> ib.values(toArray(row)));
@@ -144,20 +147,27 @@ public class Import implements Operation {
 
         private String table;
 
+        private boolean built;
+
         private Builder(String location) {
+            requireNonNull(location, "location must not be null");
             this.location = getClass().getClassLoader().getResource(location);
             if (this.location == null)
-                throw new IllegalArgumentException("specified resource was not found: " + location);
+                throw new IllegalArgumentException(location + " not found");
         }
 
         /**
          * Constructs and returns a new {@link Import} instance.
          *
          * @return a new {@link Import} instance
+         * @throws IllegalStateException if {@code table} has not been specified yet, or if this
+         * builder has built an {@link Import} already
          */
         @NotNull
         public Import build() {
-            if (table == null) throw new IllegalStateException("'table' has not been specified yet");
+            if (table == null) throw new IllegalStateException("table has not been specified yet");
+            requireNotBuilt();
+            built = true;
             return new Import(this);
         }
 
@@ -166,9 +176,12 @@ public class Import implements Operation {
          *
          * @param table a table name
          * @return the reference to this object
+         * @throws IllegalStateException if this builder has built an {@link Import} already
          */
         @NotNull
         public Builder into(@NotNull String table) {
+            requireNotBuilt();
+            requireNonNull(table, "table must not be null");
             this.table = table;
             return this;
         }
@@ -181,9 +194,12 @@ public class Import implements Operation {
          *
          * @param charset a charset
          * @return the reference to this object
+         * @throws IllegalStateException if this builder has built an {@link Import} already
          */
         @NotNull
         public Builder withCharset(@NotNull Charset charset) {
+            requireNotBuilt();
+            requireNonNull(charset, "charset must not be null");
             this.charset = charset;
             return this;
         }
@@ -196,9 +212,12 @@ public class Import implements Operation {
          *
          * @param charset a charset
          * @return the reference to this object
+         * @throws IllegalStateException if this builder has built an {@link Import} already
          */
         @NotNull
         public Builder withCharset(@NotNull String charset) {
+            requireNotBuilt();
+            requireNonNull(charset, "charset must not be null");
             this.charset = Charset.forName(charset);
             return this;
         }
@@ -211,9 +230,11 @@ public class Import implements Operation {
          *
          * @param delimiter a delimiter character
          * @return the reference to this object
+         * @throws IllegalStateException if this builder has built an {@link Import} already
          */
         @NotNull
         public Builder withDelimiter(char delimiter) {
+            requireNotBuilt();
             this.delimiter = delimiter;
             return this;
         }
@@ -226,10 +247,18 @@ public class Import implements Operation {
          *
          * @param headers headers
          * @return the reference to this object
+         * @throws IllegalStateException if this builder has built an {@link Import} already
          */
         @NotNull
-        public Builder withHeader(@NotNull Collection<String> headers) {
-            this.headers = headers.toArray(EMPTY_ARRAY);
+        public Builder withHeader(@NotNull Collection<@NotNull String> headers) {
+            requireNotBuilt();
+            requireNonNull(headers, "headers must not be null");
+            this.headers = new String[headers.size()];
+            int i = 0;
+            for (String header : headers) {
+                requireNonNull(header, "headers must not contain null");
+                this.headers[i++] = header;
+            }
             return this;
         }
 
@@ -241,10 +270,18 @@ public class Import implements Operation {
          *
          * @param headers headers
          * @return the reference to this object
+         * @throws IllegalStateException if this builder has built an {@link Import} already
          */
         @NotNull
         public Builder withHeader(@NotNull String... headers) {
-            this.headers = headers;
+            requireNotBuilt();
+            requireNonNull(headers, "headers must not be null");
+            this.headers = new String[headers.length];
+            int i = 0;
+            for (String header : headers) {
+                requireNonNull(header, "headers must not contain null");
+                this.headers[i++] = header;
+            }
             return this;
         }
 
@@ -256,9 +293,12 @@ public class Import implements Operation {
          *
          * @param nullString a string that represents {@code null} value
          * @return the reference to this object
+         * @throws IllegalStateException if this builder has built an {@link Import} already
          */
         @NotNull
         public Builder withNullAs(@NotNull String nullString) {
+            requireNotBuilt();
+            requireNonNull(nullString, "nullString must not be null");
             this.nullString = nullString;
             return this;
         }
@@ -271,11 +311,17 @@ public class Import implements Operation {
          *
          * @param quote a quotation mark
          * @return the reference to this object
+         * @throws IllegalStateException if this builder has built an {@link Import} already
          */
         @NotNull
         public Builder withQuote(char quote) {
+            requireNotBuilt();
             this.quote = quote;
             return this;
+        }
+
+        private void requireNotBuilt() {
+            if (built) throw new IllegalStateException("this operation has been built already");
         }
     }
 }
