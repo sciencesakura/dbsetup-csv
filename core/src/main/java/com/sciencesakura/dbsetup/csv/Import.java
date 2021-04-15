026c3b65ae55dad4a7b6ddca2d486909eec08d9c
sciencesakura <sakurasakurascience@gmail.com>
2021-04-16 04:04:23 +0900

    Improve APIs (#22)
    * Upgrade Gradle and dependencies
    
    * Remove Builder's state check
    
    * Improve APIs

--- core/src/main/java/com/sciencesakura/dbsetup/csv/Import.java
+++ core/src/main/java/com/sciencesakura/dbsetup/csv/Import.java
@@ -34,10 +34,12 @@ import org.apache.commons.csv.CSVRecord;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.io.Serializable;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Collection;
@@ -48,26 +50,37 @@ import static java.util.Objects.requireNonNull;
 
 /**
  * An operation which imports the CSV file into the specified table.
+ * <p>
+ * Usage:
+ * </p>
+ * <pre><code>
+ * import static com.sciencesakura.dbsetup.csv.Import.csv;
+ *
+ * // `testdata.csv` must be in classpath.
+ * Operation operation = csv("testdata.csv").build();
+ * DbSetup dbSetup = new DbSetup(destination, operation);
+ * dbSetup.launch();
+ * </code></pre>
  *
  * @author sciencesakura
  */
-public class Import implements Operation {
+public final class Import implements Operation {
 
     private static final String[] EMPTY_ARRAY = {};
 
     /**
-     * Creates a new {@code Import.CSV} instance.
+     * Creates a new {@code Import.Builder} instance.
      * <p>
      * The specified location string must be the relative path string from classpath root.
      * </p>
      *
      * @param location the location of the source file that is the relative path from classpath root
-     * @return the new {@code Import.CSV} instance
+     * @return the new {@code Import.Builder} instance
      * @throws IllegalArgumentException if the source file was not found
      */
     @NotNull
-    public static CSV csv(@NotNull String location) {
-        return new CSV(location);
+    public static Builder csv(@NotNull String location) {
+        return new Builder(location);
     }
 
     private static CSVFormat createFormat(Builder builder) {
@@ -85,6 +98,23 @@ public class Import implements Operation {
         return format;
     }
 
+    private static String getTable(Builder builder) {
+        if (builder.table == null) {
+            String table;
+            try {
+                Path filename = Paths.get(builder.location.toURI()).getFileName();
+                assert filename != null;
+                table = filename.toString();
+            } catch (URISyntaxException e) {
+                throw new DbSetupRuntimeException(e);
+            }
+            int p = table.lastIndexOf('.');
+            return p == -1 ? table : table.substring(0, p);
+        } else {
+            return builder.table;
+        }
+    }
+
     private static Object[] toArray(CSVRecord row) {
         int length = row.size();
         String[] values = new String[length];
@@ -98,14 +128,14 @@ public class Import implements Operation {
 
     private Import(Builder builder) {
         CSVFormat format = createFormat(builder);
-        Insert.Builder ib = Insert.into(builder.table);
-        try (CSVParser csv = CSVParser.parse(builder.csv.location.openStream(), builder.charset, format)) {
+        Insert.Builder ib = Insert.into(getTable(builder));
+        try (CSVParser csv = CSVParser.parse(builder.location.openStream(), builder.charset, format)) {
             ib.columns(csv.getHeaderNames().toArray(EMPTY_ARRAY));
             builder.defaultValues.forEach(ib::withDefaultValue);
             builder.valueGenerators.forEach(ib::withGeneratedValue);
             csv.forEach(row -> ib.values(toArray(row)));
         } catch (IOException e) {
-            throw new DbSetupRuntimeException("failed to open " + builder.csv.location, e);
+            throw new DbSetupRuntimeException("failed to open " + builder.location, e);
         }
         internalOperation = ib.build();
     }
@@ -116,53 +146,19 @@ public class Import implements Operation {
     }
 
     /**
-     * A representation of the CSV file.
-     *
-     * @author sciencesakura
-     */
-    public static class CSV implements Serializable {
-
-        private static final long serialVersionUID = -411937921273901442L;
-
-        private final URL location;
-
-        private CSV(String location) {
-            requireNonNull(location, "location must not be null");
-            this.location = getClass().getClassLoader().getResource(location);
-            if (this.location == null)
-                throw new IllegalArgumentException(location + " not found");
-        }
-
-        /**
-         * Create a new {@code Import.Builder} instance.
-         *
-         * @param table the table name
-         * @return the new {@code Import.Builder} instance
-         */
-        @NotNull
-        public Builder into(@NotNull String table) {
-            return new Builder(this, requireNonNull(table, "table must not be null"));
-        }
-    }
-
-    /**
      * A builder to create the {@code Import} instance.
-     * <p>
-     * This builder can be used only once. Once it has built {@code Import} instance, builder's
-     * methods will throw an {@code IllegalStateException}.
-     * </p>
      *
      * @author sciencesakura
      */
-    public static class Builder {
+    public static final class Builder {
 
         private final Map<String, Object> defaultValues = new LinkedHashMap<>();
 
         private final Map<String, ValueGenerator<?>> valueGenerators = new LinkedHashMap<>();
 
-        private final CSV csv;
+        private final URL location;
 
-        private final String table;
+        private String table;
 
         private Charset charset = StandardCharsets.UTF_8;
 
@@ -174,24 +170,36 @@ public class Import implements Operation {
 
         private char quote = '"';
 
-        private boolean built;
-
-        private Builder(CSV csv, String table) {
-            this.csv = csv;
-            this.table = table;
+        private Builder(String location) {
+            requireNonNull(location, "location must not be null");
+            this.location = getClass().getClassLoader().getResource(location);
+            if (this.location == null) {
+                throw new IllegalArgumentException(location + " not found");
+            }
         }
 
         /**
          * Build a new {@code Import} instance.
          *
          * @return the new {@code Import} instance
-         * @throws IllegalStateException if this builder has built an {@code Import} already
          */
         @NotNull
         public Import build() {
-            requireNotBuilt();
-            built = true;
             return new Import(this);
+        }
+
+        /**
+         * Specifies a table name to insert into.
+         * <p>
+         * By default the name of source file (without extension) is used.
+         * </p>
+         *
+         * @param table the table name
+         * @return the reference to this object
+         */
+        public Builder into(@NotNull String table) {
+            this.table = requireNonNull(table, "table must not be null");
+            return this;
         }
 
         /**
@@ -202,11 +210,8 @@ public class Import implements Operation {
          *
          * @param charset the charset
          * @return the reference to this object
-         * @throws IllegalStateException if this builder has built an {@code Import} already
          */
-        @NotNull
         public Builder withCharset(@NotNull Charset charset) {
-            requireNotBuilt();
             this.charset = requireNonNull(charset, "charset must not be null");
             return this;
         }
@@ -219,11 +224,8 @@ public class Import implements Operation {
          *
          * @param charset the charset name
          * @return the reference to this object
-         * @throws IllegalStateException if this builder has built an {@code Import} already
          */
-        @NotNull
         public Builder withCharset(@NotNull String charset) {
-            requireNotBuilt();
             requireNonNull(charset, "charset must not be null");
             this.charset = Charset.forName(charset);
             return this;
@@ -235,11 +237,8 @@ public class Import implements Operation {
          * @param column the column name
          * @param value  the default value
          * @return the reference to this object
-         * @throws IllegalStateException if this builder has built an {@code Import} already.
          */
-        @NotNull
         public Builder withDefaultValue(@NotNull String column, Object value) {
-            requireNotBuilt();
             requireNonNull(column, "column must not be null");
             defaultValues.put(column, value);
             return this;
@@ -253,11 +252,8 @@ public class Import implements Operation {
          *
          * @param delimiter the delimiter character
          * @return the reference to this object
-         * @throws IllegalStateException if this builder has built an {@code Import} already
          */
-        @NotNull
         public Builder withDelimiter(char delimiter) {
-            requireNotBuilt();
             this.delimiter = delimiter;
             return this;
         }
@@ -268,11 +264,8 @@ public class Import implements Operation {
          * @param column         the column name
          * @param valueGenerator the generator
          * @return the reference to this object
-         * @throws IllegalStateException if this builder has built an {@code Import} already
          */
-        @NotNull
         public Builder withGeneratedValue(@NotNull String column, @NotNull ValueGenerator<?> valueGenerator) {
-            requireNotBuilt();
             requireNonNull(column, "column must not be null");
             requireNonNull(valueGenerator, "valueGenerator must not be null");
             valueGenerators.put(column, valueGenerator);
@@ -287,11 +280,8 @@ public class Import implements Operation {
          *
          * @param headers the header names
          * @return the reference to this object
-         * @throws IllegalStateException if this builder has built an {@code Import} already
          */
-        @NotNull
         public Builder withHeader(@NotNull Collection<@NotNull String> headers) {
-            requireNotBuilt();
             requireNonNull(headers, "headers must not be null");
             this.headers = new String[headers.size()];
             int i = 0;
@@ -309,11 +299,8 @@ public class Import implements Operation {
          *
          * @param headers the header names
          * @return the reference to this object
-         * @throws IllegalStateException if this builder has built an {@code Import} already
          */
-        @NotNull
         public Builder withHeader(@NotNull String... headers) {
-            requireNotBuilt();
             requireNonNull(headers, "headers must not be null");
             this.headers = new String[headers.length];
             int i = 0;
@@ -331,11 +318,8 @@ public class Import implements Operation {
          *
          * @param nullString the string that represents {@code null} value
          * @return the reference to this object
-         * @throws IllegalStateException if this builder has built an {@code Import} already
          */
-        @NotNull
         public Builder withNullAs(@NotNull String nullString) {
-            requireNotBuilt();
             this.nullString = requireNonNull(nullString, "nullString must not be null");
             return this;
         }
@@ -348,17 +332,10 @@ public class Import implements Operation {
          *
          * @param quote the quotation mark
          * @return the reference to this object
-         * @throws IllegalStateException if this builder has built an {@code Import} already
          */
-        @NotNull
         public Builder withQuote(char quote) {
-            requireNotBuilt();
             this.quote = quote;
             return this;
-        }
-
-        private void requireNotBuilt() {
-            if (built) throw new IllegalStateException("this operation has been built already");
         }
     }
 }
