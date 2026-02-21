caa81d3ad66f40dc267780fce66aee235c1476cd
sciencesakura <sciencesakura@users.noreply.github.com>
2026-02-21 10:43:36 +0000

    Deploying to gh-pages from @ sciencesakura/dbsetup-csv@07522e0afad2cc2e002cb01cb4b17122895938e4 🚀

--- apidocs/type-search-index.js
+++ apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.sciencesakura.dbsetup.csv","l":"Import.Builder"},{"p":"com.sciencesakura.dbsetup.csv","l":"Import"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.sciencesakura.dbsetup.csv","l":"Import.Builder"},{"p":"com.sciencesakura.dbsetup.csv","l":"Import"}];updateSearchResults();
