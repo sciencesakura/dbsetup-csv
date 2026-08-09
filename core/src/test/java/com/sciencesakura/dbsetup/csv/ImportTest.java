12d4c3daa640ea1d19e1e272dc7a70e7d8b289bb
sciencesakura <sakurasakurascience@gmail.com>
2026-08-09 17:00:17 +0900

    feat: Use package-level nullability annotation.

--- core/src/test/java/com/sciencesakura/dbsetup/csv/ImportTest.java
+++ core/src/test/java/com/sciencesakura/dbsetup/csv/ImportTest.java
@@ -27,10 +27,12 @@ import java.util.UUID;
 import org.assertj.db.type.AssertDbConnection;
 import org.assertj.db.type.AssertDbConnectionFactory;
 import org.assertj.db.type.Changes;
+import org.jspecify.annotations.NullUnmarked;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@NullUnmarked
 class ImportTest {
 
   AssertDbConnection connection;
